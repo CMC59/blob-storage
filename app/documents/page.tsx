e182b6fa580daa03bda4c67a5d0db728210945af
Philippe Plaïa <philippeplaia@gmail.com>
2025-06-19 17:25:40 +0200

    import components ui and uploader

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Pencil, Trash2, Eye } from 'lucide-react';
+import styles from './documents.module.scss';
 import {
   Table,
   TableBody,
@@ -7,26 +12,43 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import Uploader from "../upload/page";
 
 export default function DocumentsPage() {
-    return      <Table>
-            <TableCaption>Liste des documents importés</TableCaption>
-            <TableHeader>
-                <TableRow>
-                <TableHead className="w-[100px]">Nom du document</TableHead>
-                <TableHead>Extension du fichier</TableHead>
-                <TableHead>Nom du document</TableHead>
-                <TableHead className="text-right">Actions</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <TableRow>
-                <TableCell className="font-medium">INV001</TableCell>
-                <TableCell>Paid</TableCell>
-                <TableCell>Credit Card</TableCell>
-                <TableCell className="text-right">$250.00</TableCell>
-                </TableRow>
-            </TableBody>
-            </Table>
-
+    return  (
+        <>
+            <div>
+                <Table>
+                    <TableCaption className={styles.caption}>Liste des documents importés</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className={styles.tablehead}>Date import du document</TableHead>
+                            <TableHead className={styles.tablehead}>Extension du fichier</TableHead>
+                            <TableHead className={styles.tablehead}>Nom du document</TableHead>
+                            <TableHead className={styles.tablehead}>Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="font-medium"></TableCell>
+                            <TableCell></TableCell>
+                            <TableCell></TableCell>
+                            <TableCell className="text-left">
+                                <Button variant="outline"><Eye color="black" size={48} /></Button>
+                                <Button variant="outline"><Pencil color="black" size={64} /></Button>
+                                <Button variant="outline"><Trash2 color="black" size={64} /></Button>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="grid w-full max-w-sm items-center gap-3">
+                <Label htmlFor="picture">Picture</Label>
+                <Input id="picture" type="file" />
+            </div>
+            <div className={`flex justify-center ${styles.uploader}`}>
+                <Uploader />
+            </div>
+        </>
+    );    
 }
