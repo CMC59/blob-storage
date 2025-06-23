9febeaf67e63cf35e61ac404e8158a353a8d91c2
Philippe Plaïa <philippeplaia@gmail.com>
2025-06-23 15:24:29 +0200

    Add login page and adapt documents page

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Pencil, Trash2, Eye } from 'lucide-react';
+import { Pencil, Trash2, Eye, FileDown } from 'lucide-react';
 import styles from './documents.module.scss';
 import {
   Table,
-  TableBody,
+  TableBody, 
   TableCaption,
   TableCell,
   TableHead,
@@ -21,7 +21,7 @@ export default async function DocumentsPage() {
      const response = await list();
     return  (
         <>
-            <div>
+            <div className="w-full max-w-6xl p-7 bg-white rounded shadow">
                 <Table>
                     <TableCaption className={styles.caption}>Liste des documents importés</TableCaption>
                     <TableHeader>
@@ -33,32 +33,28 @@ export default async function DocumentsPage() {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        <TableRow>
+                        {response.blobs.map((blob) => (
+                            <TableRow key={blob.pathname}>
                             <TableCell className="font-medium"></TableCell>
-                            <TableCell></TableCell>
-                            <TableCell></TableCell>
+                            <TableCell>{blob.pathname.split('.').pop()}</TableCell>
+                            <TableCell>{blob.pathname}</TableCell>
                             <TableCell className="text-left">
-                                <Button variant="outline"><Eye color="black" size={48} /></Button>
-                                <Button variant="outline"><Pencil color="black" size={64} /></Button>
                                 <Button variant="outline"><Trash2 color="black" size={64} /></Button>
+                                <a href={blob.downloadUrl} download>
+                                <Button variant="outline">
+                                    <FileDown color="black" size={32} />
+                                </Button>
+                                </a>                            
                             </TableCell>
-                        </TableRow>
-                    </TableBody>
+                            </TableRow>
+                        ))}
+                        </TableBody>
                 </Table>
+                <p>LOL</p>
             </div>
-            <div className="grid w-full max-w-sm items-center gap-3">
+            <div className="grid w-full max-w-sm items-center gap-3 p-6">
                 <Label htmlFor="picture">Picture</Label>
                 <Input id="picture" type="file" />
-            </div>
-            <div className={`flex justify-center ${styles.uploader}`}>
-                <Uploader />
-            </div>
-            <div>
-                {response.blobs.map((blob) => (
-                    <a key={blob.pathname} href={blob.downloadUrl}>
-                    {blob.pathname}
-                    </a>
-                ))}
             </div>
         </>
     );    
